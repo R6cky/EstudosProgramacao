c8cffd84de4cb961a89824b922fdf33a1d47c737
r6cky <cleitonrocha321@hotmail.com>
2025-01-23 18:11:30 -0300

    add content about object

--- JavaScript/objetos/index.js
+++ JavaScript/objetos/index.js
@@ -4,10 +4,22 @@ const pessoa = {
   sobrenome: "frizzer",
   idade: "35",
 };
+//--------------------------------------------
+//--------------------------------------------
+function Carro(cor, portas) {
+  this.cor = cor;
+  this.portas = portas;
+  Object.freeze(this); // Trava todo o objeto, fica imultável
+}
+
+const carro1 = new Carro("vermelha", "4 portas");
+carro1.combustivel = "gasolina";
+console.log(carro1);
 
 console.log(pessoa.nome);
 console.log(pessoa["nome"]);
-
+//--------------------------------------------
+//--------------------------------------------
 //Declaração com contructor
 const pessoa2 = new Object();
 pessoa2.falarnome = function () {
@@ -15,10 +27,30 @@ pessoa2.falarnome = function () {
 };
 console.log(pessoa2.falarnome());
 
-//Travar/ bloquear objetos para
-
-Object.freeze(pessoa);
+Object.freeze(pessoa); //Travar/ bloquear
 
 pessoa.nasciment = "24/05/1985";
 
-console.log(pessoa.nasciment);
+console.log(pessoa.nasciment); // O valor do objeto continua o mesmo.
+
+//--------------------------------------------
+//--------------------------------------------
+
+// Travando de forma personalizada um objeto
+
+function Notebook(processador, ram) {
+  this.processador = processador;
+  this.ram = ram;
+
+  Object.defineProperty(this, "estoque", {
+    value: "100",
+    enumerable: true,
+    writable: true,
+    configurable: false,
+  });
+}
+
+const mynotebook = new Notebook("i7", "16");
+mynotebook.estoque = "456";
+delete mynotebook.estoque;
+console.log(mynotebook);
